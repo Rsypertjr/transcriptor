2391452aae318082f2cdca9563114fca8ed7741c
vagrant <vagrant@homestead>
2017-01-20 18:32:35 +0000

    edit database.php file

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_902535c1e5b98f0'),
+            'username' => env('DB_USERNAME', 'b8d8ba055a1aed'),
+            'password' => env('DB_PASSWORD', 'dc8e511e'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
