431bc2b14b88b8ad99aadfe4dbce8748a0559ff1
vagrant <vagrant@homestead>
2017-01-20 18:16:57 +0000

    adding view and route files

---
+++ resources/views/productSubmit.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+		<link href="/css/app.css" rel="stylesheet">
+		
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+			  
+                  
+				$('table tr th:nth-child(6) button').css('display','none');	
+				$('table tr th:nth-child(6) span').css('display','block');	
+				$('tr td:nth-child(6)').on('mousedown',function(){
+					// name attribute contains id of product
+					
+					$(this).on('mouseup',function(){
+						if($('table tr td:contains("Yes")').length > 0)	{
+							$('table tr th:nth-child(6) button').css('display','block');
+							$('table tr th:nth-child(6) span').css('display','none');
+							
+						}
+						if($('table tr td:contains("Yes")').length == 0){
+								$('table tr th:nth-child(6) span').css('display','block').text('Want to Edit?');
+								$('table tr th:nth-child(6) button').css('display','none');
+						}
+					});
+					
+					
+					if($(this).text() == 'No, but Click for Yes')				
+						$(this).text('Yes, but Click for No');
+					else if($(this).text() == 'Yes, but Click for No')
+						$(this).text('No, but Click for Yes');
+			
+				});
+				$('form#edits button').on('click',function(){				        
+                        $('table tr td:contains("Yes, but Click")').each(function(){
+							 var id = $(this).attr("name");
+							 $('form#edits')
+							   .append('<input type="hidden" name="id[]" value="'+ id +'">');
+						 });
+						 
+						 $('form#edits').submit();
+					
+					});
+					
+				$('form#deleteItem button').on('click',function(){				        
+                         var name = $('form input#name').val()
+						 $('form#deleteItem')
+						   .append('<input type="hidden" name="name" value="'+ name +'">');
+					
+						 
+						 $('form#deleteItem').submit();
+					
+					});	
+			
+			});
+		</script>
+      
+    </head>
+    <body>      
+
+		<h2 style="position:relative;width:auto;margin-left:45%">Laravel Test</h2>
+	   
+        <div class="content">
+				 @if($edit == 'false')
+					<div class="container">
+						<form action="/submit" method="post">
+							{!! csrf_field() !!}
+							<div class="form-group">
+								<label for="title">Product Name</label>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+							</div>
+							<div class="form-group">
+								<label for="title">Quantity in Stock</label>
+								<input type="number" class="form-control" id="quantity" name="quantity" placeholder="Quantity">
+							</div>
+							<div class="form-group">
+								<label for="title">Price per Item</label>
+								<input type="number" step="any" class="form-control" id="price" name="price" placeholder="Price">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+						</form>
+					 </div>	
+					 <br><br>
+				@endif
+		        @if($edit == 'true')
+					<div class="container">
+						<form action="/update" method="post">
+							{!! csrf_field() !!}
+							<div class="form-group">
+								<label for="title">Edit Product Name</label>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name" value="{{$edProd->name}}"}">
+							</div>
+							<div class="form-group">
+								<label for="title">Edit Quantity in Stock</label>
+								<input type="number" class="form-control" id="quantity" name="quantity" placeholder="Quantity" value="{{$edProd->quantityInStock}}">
+							</div>
+							<div class="form-group">
+								<label for="title">Edit Price per Item</label>
+								<input type="number" step="any" class="form-control" id="price" name="price" placeholder="Price" value="{{$edProd->pricePerItem}}">
+								<input type="hidden" name="pId" value="{{$edProd->id}}">
+							</div>
+							<button type="submit" class="btn btn-default">Submit Edit</button>
+							
+						</form>
+						
+						<form id="deleteItem" style="position:relative;float:left;margin-top:-2.55em;margin-left:10em" id="delInput" action="/delete" method="post">
+								{!! csrf_field() !!}
+								<button class="btn btn-default">Delete this Item</button>
+						</form>
+						<br>
+						<form id="moreInput" action="/" method="get">
+								{!! csrf_field() !!}
+								<button type="submit" class="btn btn-default">Back to More Input</button>
+						</form>
+					 </div>	
+					 <br><br>
+				@endif
+				@if (count($products) > 0)
+					<div class="row">
+						<div class="col-md-10 col-md-offset-1">
+							<div class="panel panel-success">
+								<div class="panel-heading">Product Data</div>						
+									<!-- Table -->
+									<table class="table table-bordered table-striped">
+										<tr>
+											<th>Product Name</th>
+											<th>Quantity In Stock</th>								
+											<th>Price per Item</th>								
+											<th>Datetime Submitted</th>								
+											<th>Total Value</th>
+											<th>
+												<form id="edits" action="/edit" method="post">
+													{!! csrf_field() !!}
+													<button>Click Here to Edit!</button>
+													<span>Want to Edit?</span></th>
+												</form>
+										</tr>
+									@foreach($products as $product)
+										<tr>
+											<td>{{ $product->name}}</td>
+											<td>{{ $product->quantityInStock }}</td>
+											<td>{{ $product->pricePerItem }}</td>
+											<td>{{ $product->updated_at }}</td>
+											<td>{{ $product->totalValue }}</td>
+											<td id ="td".{{ $product->id}} name="{{ $product->id }}">No, but Click for Yes</td>
+										</tr>
+									@endforeach
+						</table>				
+								</div>
+							</div>
+						</div>
+					</div>
+				@endif
+			
+        </div>
+		
+	<!--<meta name="_token" content="{!! csrf_token() !!}" /> -->
+	<meta name="csrf-token" content="{{ csrf_token() }}" />
+    </body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +11,110 @@
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+	$products = \App\Product::all();
+	$edit = "false";
+    return view('productSubmit',compact('products'),['edit' => $edit]);
+});
+
+
+Route::post('/submit', function(Request $request){
+/*	$validator = Validator::make($request->all(),[
+		'name' => 'required',
+		'quantityInStock' => 'required',
+		'pricePerItem' => 'required',
+	]);
+	
+	if($validator->fails()){
+		return back()
+			->withInput()
+			->withErrors($validator);
+	}  */
+		
+	$product = new \App\Product;
+    $product->name = $request->input('name');
+    $qt = $product->quantityInStock = $request->input('quantity');
+    $ppI = $product->pricePerItem = $request->input('price');	
+	$product->totalValue = $qt * $ppI;	
+	$product->editing = 'No';
+	$checkForName = \App\Product::where('name',$product->name)->get();
+	
+	if(count($checkForName) > 0)
+		\App\Product::where('name',$product->name)->update(array('name' => $product->name));
+    else		
+		$product->save();
+	
+	$products =  App\Product::all();
+	$productsJS = $products->toJson();
+	file_put_contents('JSONfile', json_encode($productsJS,TRUE));
+	
+	$edit = "false";
+    return view('productSubmit',compact('products'),['edit' => $edit]);
+		
+});
+
+Route::post('/update',function(Request $request){
+	$pname = $request->input('name');
+	$pQty = $request->input('quantity');
+	$pPpi = $request->input('price');
+	$pId = $request->input('pId');
+	\App\Product::where('id',$pId)->update(array('quantityInStock' => $pQty));
+	\App\Product::where('id',$pId)->update(array('pricePerItem' => $pPpi));
+	\App\Product::where('id',$pId)->update(array('name' => $pname));
+	//\App\Product::where('id',$pId)->update(array('editing' => 'No'));
+	
+	$edit = "true";
+	$products = \App\Product::all();
+	$edProds = \App\Product::where('editing','Yes')->get();
+	foreach($edProds as $edProd){
+			$edit = "true";	
+			return view('productSubmit',compact('products','edProd'),['edit' => $edit]);	
+	    }
+});
+
+Route::post('/delete',function(Request $request){
+	$pname = $request->input('name');
+	//$pQty = $request->input('quantity');
+	//$pPpi = $request->input('price');
+	//$pId = $request->input('pId');
+	//\App\Product::where('id',$pId)->update(array('quantityInStock' => $pQty));
+	//\App\Product::where('id',$pId)->update(array('pricePerItem' => $pPpi));
+	\App\Product::where('name',$pname)->delete();
+	//\App\Product::where('id',$pId)->update(array('editing' => 'No'));
+	
+	$edit = "true";
+	$products = \App\Product::all();
+	$edProds = \App\Product::where('editing','Yes')->get();
+	if(count($edProds ) > 0){
+					foreach($edProds as $edProd){
+							$edit = "true";	
+							return view('productSubmit',compact('products','edProd'),['edit' => $edit]);	
+						}
+					}
+	else{
+		$edit = "false";
+		return view('productSubmit',compact('products'),['edit' => $edit]);
+	}
+});
+
+Route::post('/edit', function(Request $request){
+	$productIds = $request->input("id");
+
+	$edit = "false";
+	foreach($productIds as $key=>$value){				
+				$edProds = \App\Product::where('id',$value)->get();
+				
+				\App\Product::where('id',$value)
+							->update(array('editing' => 'Yes'));
+					
+				$products = \App\Product::all();
+				foreach($edProds as $edProd){
+					    $edit = "true";	
+						return view('productSubmit',compact('products','edProd'),['edit' => $edit]);	
+				}
+			}
+	   
+	
 });
