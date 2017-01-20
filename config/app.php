35318ca9f8d96c03bc85df765e6199b8df46b531
vagrant <vagrant@homestead>
2017-01-20 18:10:13 +0000

    deploying basic app

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://safe-bayou-92699.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
