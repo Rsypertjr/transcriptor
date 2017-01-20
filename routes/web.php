36138e71734c4f1d47698c0659fe7d0b7f64a1bb
vagrant <vagrant@homestead>
2017-01-20 18:50:07 +0000

    debugging web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/', function () {
-	$products = \App\Product::all();
+	//$products = \App\Product::all();
 	$edit = "false";
 	//return view('productSubmit',compact('products'),['edit' => $edit]);	
 	return view('welcome');
