50bb94fb3d2cffabb6afab5dd1303af7b2e55e50
vagrant <vagrant@homestead>
2017-01-20 20:14:50 +0000

    I forgot Product Model file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Http\Request;
-use App\Repositories\ProductRepository;
+use App\Product;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,10 +18,10 @@ Route::get('/', function () {
 });
 
 Route::get('/', function () {
-	$products =  new ProductRepository();
+	$products = App\Product::all();
 	$edit = "false";
-	//return view('productSubmit',compact('products'),['edit' => $edit]);	
-	return view('welcome');
+	return view('productSubmit',compact('products'),['edit' => $edit]);	
+	//return view('welcome');
 });
 
 
@@ -38,20 +38,20 @@ Route::post('/submit', function(Request $request){
 			->withErrors($validator);
 	}  */
 		
-	$product = new Product;
+	$product = new App\Product;
     $product->name = $request->input('name');
     $qt = $product->quantityInStock = $request->input('quantity');
     $ppI = $product->pricePerItem = $request->input('price');	
 	$product->totalValue = $qt * $ppI;	
 	$product->editing = 'No';
-	$checkForName = Product::where('name',$product->name)->get();
+	$checkForName = App\Product::where('name',$product->name)->get();
 	
 	if(count($checkForName) > 0)
-		Product::where('name',$product->name)->update(array('name' => $product->name));
+		App\Product::where('name',$product->name)->update(array('name' => $product->name));
     else		
 		$product->save();
 	
-	$products =  Product::all();
+	$products =  App\Product::all();
 	$productsJS = $products->toJson();
 	file_put_contents('JSONfile', json_encode($productsJS,TRUE));
 	
@@ -65,14 +65,14 @@ Route::post('/update',function(Request $request){
 	$pQty = $request->input('quantity');
 	$pPpi = $request->input('price');
 	$pId = $request->input('pId');
-	Product::where('id',$pId)->update(array('quantityInStock' => $pQty));
-	Product::where('id',$pId)->update(array('pricePerItem' => $pPpi));
-	Product::where('id',$pId)->update(array('name' => $pname));
-	//Product::where('id',$pId)->update(array('editing' => 'No'));
+	App\Product::where('id',$pId)->update(array('quantityInStock' => $pQty));
+	App\Product::where('id',$pId)->update(array('pricePerItem' => $pPpi));
+	App\Product::where('id',$pId)->update(array('name' => $pname));
+	//App\Product::where('id',$pId)->update(array('editing' => 'No'));
 	
 	$edit = "true";
-	$products = Product::all();
-	$edProds = Product::where('editing','Yes')->get();
+	$products = App\Product::all();
+	$edProds = App\Product::where('editing','Yes')->get();
 	foreach($edProds as $edProd){
 			$edit = "true";	
 			return view('productSubmit',compact('products','edProd'),['edit' => $edit]);	
@@ -84,14 +84,14 @@ Route::post('/delete',function(Request $request){
 	//$pQty = $request->input('quantity');
 	//$pPpi = $request->input('price');
 	//$pId = $request->input('pId');
-	//Product::where('id',$pId)->update(array('quantityInStock' => $pQty));
-	//Product::where('id',$pId)->update(array('pricePerItem' => $pPpi));
-	Product::where('name',$pname)->delete();
-	//Product::where('id',$pId)->update(array('editing' => 'No'));
+	//App\Product::where('id',$pId)->update(array('quantityInStock' => $pQty));
+	//App\Product::where('id',$pId)->update(array('pricePerItem' => $pPpi));
+	App\Product::where('name',$pname)->delete();
+	//App\Product::where('id',$pId)->update(array('editing' => 'No'));
 	
 	$edit = "true";
-	$products = Product::all();
-	$edProds = Product::where('editing','Yes')->get();
+	$products = App\Product::all();
+	$edProds = App\Product::where('editing','Yes')->get();
 	if(count($edProds ) > 0){
 					foreach($edProds as $edProd){
 							$edit = "true";	
@@ -109,12 +109,12 @@ Route::post('/edit', function(Request $request){
 
 	$edit = "false";
 	foreach($productIds as $key=>$value){				
-				$edProds = Product::where('id',$value)->get();
+				$edProds = App\Product::where('id',$value)->get();
 				
-				Product::where('id',$value)
+				App\Product::where('id',$value)
 							->update(array('editing' => 'Yes'));
 					
-				$products = Product::all();
+				$products = App\Product::all();
 				foreach($edProds as $edProd){
 					    $edit = "true";	
 						return view('productSubmit',compact('products','edProd'),['edit' => $edit]);	
