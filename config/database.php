425d837ae2448d262504264780fdf160652c9070
vagrant <vagrant@homestead>
2017-02-09 04:11:07 +0000

    heroku deploy, more PhpUnit, preparing for selenium

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql2'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
