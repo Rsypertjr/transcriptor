e6b535b9e798b624a075d7515e0c56c462f332bd
vagrant <vagrant@homestead>
2017-02-16 06:56:59 +0000

    heroku config settings

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql3'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
